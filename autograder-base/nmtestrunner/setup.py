1ed7e2c075b65fef52163d2ab1cc8b6a9ce72ec4
matt russell <mrussell@cs.tufts.edu>
2022-03-03 11:53:31 -0500

    github main

---
+++ autograder-base/nmtestrunner/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='nmtestrunner',
+    author='',
+    url='',
+    version='1.0.0',
+    package_dir={'nmtestrunner': 'src'},
+    packages=['nmtestrunner'],
+    description="Scaffolding for Noah Mendelsohn's Testing Framework",
+    install_requires=['tqdm'],
+)
