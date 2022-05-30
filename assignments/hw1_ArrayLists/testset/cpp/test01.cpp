2c33ea1cb0a831f537f7f0d176f6f4d0026370ac
matt russell <mrussell@cs.tufts.edu>
2022-05-30 11:57:56 -0400

    testset for hw1 now has full tests

--- assignments/hw1_ArrayLists/testset/cpp/test01.cpp
+++ assignments/hw1_ArrayLists/testset/cpp/test01.cpp
@@ -1,17 +1,17 @@
 /**
  * CS15 HW1 TEST 01
- *
+ * 
  * Tests Default Constructor ArrayList
- *
+ * 
  * abui02
  * 9/23/2021
- *
+ * 
  * mchamp03 - 01/25/2022
  * - made consistent with SP 2022 spec
  */
 
-#include "CharArrayList.h"
 #include <iostream>
+#include "CharArrayList.h"
 
 int main() {
     CharArrayList list1;
