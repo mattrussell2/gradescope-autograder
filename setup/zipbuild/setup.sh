f869b418d6f290f626047cd6312dbf39b0aa2bb6
matt russell <mrussell@cs.tufts.edu>
2022-05-31 15:39:34 -0400

    fix zipbuild setup.sh bug

--- setup/zipbuild/setup.sh
+++ setup/zipbuild/setup.sh
@@ -23,10 +23,10 @@ python3.9 -m pip install --upgrade pip
 python3.9 -m pip install toml dataclasses tqdm filelock python_dateutil
 
 # copy the config file
-cp /autograder/source/setup/config.ini /autograder/source/config.ini
+cp /autograder/source/setup/autograder_config.ini /autograder/source/config.ini
 
 # update message of the day
-cp /autograder/source/lib/motd /etc/motd
+cp /autograder/source/etc/motd /etc/motd
 
 # copy DiffHighlight.pm so diff-so-fancy can view it
 cp /autograder/source/lib/DiffHighlight.pm /usr/share/perl5/
@@ -38,7 +38,7 @@ cp /autograder/source/bin/run_autograder /autograder/
 printf 'export PS1=\"\\u@gs:\\W\\$ \"\n' >> ~/.bashrc
 
 # load the config vars so we have access to $REPO_REMOTE_PATH
-source /autograder/source/config.ini
+source /autograder/source/autograder_config.ini
 
 # clone the course repo
 cd /autograder/source && git clone $REPO_REMOTE_PATH course-repo
