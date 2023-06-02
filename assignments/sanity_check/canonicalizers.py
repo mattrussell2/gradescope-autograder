7e093a4b2b9b537b0a99844eb2780e64bb4ffb3f
Matthew P. Russell <mrussell@eecs.tufts.edu>
2023-06-02 15:16:09 +0000

    update sanity_check example

--- assignments/sanity_check/canonicalizers.py
+++ assignments/sanity_check/canonicalizers.py
@@ -1,10 +1,10 @@
 from pathlib import Path 
 
-def do_nothing(output, *args):
-    return output
+def do_nothing(student_unccd_output, reference_unccd_output, testname, streamname, params):
+    return student_unccd_output
 
-def sort_lines(student_output, *args):
-    return '\n'.join(sorted(student_output.splitlines()))
+def sort_lines(student_unccd_output, reference_unccd_output, testname, streamname, params):
+    return '\n'.join(sorted(student_unccd_output.splitlines()))
 
-def sort_num_lines(student_output, *args):
-    return '\n'.join([str(x) for x in sorted([int(x) for x in student_output.splitlines()])])
+def sort_num_lines(student_unccd_output, reference_unccd_output, testname, streamname, params):
+    return '\n'.join([str(x) for x in sorted([int(x) for x in student_unccd_output.splitlines()])])
