3f87e9ecaf3b66edff5069084f8ee6bf7081a424
Matt Russell <mrussell@cs.tufts.edu>
2023-06-05 14:27:48 +0000

    valgrind to false by default with self.exec_command

--- bin/autograde.py
+++ bin/autograde.py
@@ -253,6 +253,9 @@ class Test:
         if self.max_ram != -1:
             self.max_ram *= 1024
 
+        if not self.exec_command:
+            self.valgrind = False
+
         # can only get here if the test has compiled
         self.compiled = True if not self.exec_command else None
 
